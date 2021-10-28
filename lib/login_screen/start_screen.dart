de367ad090e3e4fb014c01acc69cf21615d247aa
Vyacheslav Fedotov <vyacheslavfedotov@MacBook-Air-Vyacheslav.local>
2021-10-28 18:53:00 +0300

    done fronted first screen

--- lib/login_screen/start_screen.dart
+++ lib/login_screen/start_screen.dart
@@ -13,29 +13,125 @@ class _StartScreenState extends State<StartScreen> {
     return MaterialApp(
       theme: ThemeData.light(),
       home: Scaffold(
+        backgroundColor: Colors.white,
         body: Form(
           child: SafeArea(
-            minimum: const EdgeInsets.all(32.0),
+            minimum: const EdgeInsets.all(28.0),
             child: Column(
               children: [
+                const SizedBox(
+                  height: 175,
+                ),
                 Image.asset(
                   'assets/images/VK.png',
-                  width: 80,
-                  height: 80,
+                  width: 100,
+                  height: 100,
                   alignment: Alignment.center,
                 ),
-                ElevatedButton(
+                const SizedBox(
+                  height: 175,
+                ),
+                OutlinedButton(
                     style: ButtonStyle(
                         backgroundColor:
                             MaterialStateProperty.all<Color>(Colors.green),
                         // padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
                         //     const EdgeInsets.symmetric(horizontal: 100))
                         fixedSize: MaterialStateProperty.all<Size>(
-                            const Size(365, 16))),
+                            const Size(365, 45)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8)))),
                     onPressed: () {},
                     child: const Text(
                       'Зарегистрироваться',
-                      style: TextStyle(color: Colors.white, fontSize: 16.0),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w400),
+                    )),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Text('или с помощью',
+                    style: TextStyle(color: Colors.grey, fontSize: 16.0)),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: Image.asset(
+                        'assets/images/fbLogo.png',
+                        height: 25,
+                      ),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                          fixedSize: MaterialStateProperty.all<Size>(
+                              const Size(170, 45)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8)))),
+                    ),
+                    const SizedBox(width: 15),
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: Image.asset(
+                        'assets/images/aplLogo.png',
+                        height: 25,
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.white),
+                        fixedSize: MaterialStateProperty.all<Size>(
+                            const Size(170, 45)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8))),
+                        side: MaterialStateProperty.all<BorderSide>(
+                            const BorderSide(width: 0.2, color: Colors.grey)),
+                        shadowColor:
+                            MaterialStateProperty.all<Color>(Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 85,
+                ),
+                const Text('Уже есть аккаунт?',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16.0,
+                    )),
+                const SizedBox(
+                  height: 25,
+                ),
+                OutlinedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blueAccent),
+                      // padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                      //     const EdgeInsets.symmetric(horizontal: 100))
+                      fixedSize:
+                          MaterialStateProperty.all<Size>(const Size(365, 45)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8))),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Войти',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w400),
                     ))
               ],
             ),
