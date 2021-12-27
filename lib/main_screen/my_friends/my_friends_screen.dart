09b2cc25e10ef68265477b88f0ff924a6be86afd
Fedotov Vyacheslav <fedotov95152@gmail.com>
2021-12-27 22:56:05 +0300

    made the first version of the friend's card

--- lib/main_screen/my_friends/my_friends_screen.dart
+++ lib/main_screen/my_friends/my_friends_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
 
 class MyFriendsScreen extends StatefulWidget {
   const MyFriendsScreen({Key? key}) : super(key: key);
@@ -7,15 +8,75 @@ class MyFriendsScreen extends StatefulWidget {
   _MyFriendsScreenState createState() => _MyFriendsScreenState();
 }
 
+buildUserProfile() {
+  @required
+  String name;
+  ImageProvider image;
+}
+
 class _MyFriendsScreenState extends State<MyFriendsScreen> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: 5,
-      itemExtent: 160,
+      itemExtent: 100,
       itemBuilder: (BuildContext context, int index) {
-        return Row(
-          children: [],
+        return Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 10.0,
+          ),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.blue[50],
+                radius: 40.0,
+                backgroundImage: const AssetImage('image/photo/foto.jpg'),
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              Container(
+                width: 175,
+                height: 75,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Юлия Рудакова',
+                      style: TextStyle(fontSize: 20),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Нижний Новгород',
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(LineIcons.phone),
+                color: Colors.blueAccent[400],
+                iconSize: 32,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(LineIcons.facebookMessenger),
+                color: Colors.blueAccent[400],
+                iconSize: 32,
+              ),
+            ],
+          ),
         );
       },
     );
