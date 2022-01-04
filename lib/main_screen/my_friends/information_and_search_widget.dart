5b3e643226652d237a23d3c9cf314e096d063bf3
Fedotov Vyacheslav <fedotov95152@gmail.com>
2022-01-04 19:22:44 +0300

    add information + search widget done + buttons QR/friend  60% done v2

--- lib/main_screen/my_friends/information_and_search_widget.dart
+++ lib/main_screen/my_friends/information_and_search_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
+import 'package:line_icons/line_icons.dart';
 
 class InformationAndSearchWidget extends StatefulWidget {
   const InformationAndSearchWidget({Key? key}) : super(key: key);
@@ -14,18 +16,21 @@ class _InformationAndSearchWidgetState
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 30.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 30.0),
       child: Column(
         children: [
+          // Аватар + "Друзья"
           Row(
+            // Аватар
             children: const [
               CircleAvatar(
                 backgroundImage: AssetImage('image/photo/foto.jpg'),
                 radius: 18.0,
               ),
               SizedBox(
-                width: 115,
+                width: 100,
               ),
+              // "Друзья"
               Text(
                 'Друзья',
                 style: TextStyle(
@@ -38,18 +43,109 @@ class _InformationAndSearchWidgetState
           const SizedBox(
             height: 30,
           ),
+          // Виджет поиска
           TextFormField(
-            decoration: const InputDecoration(
-                label: Icon(
-                  Icons.search,
-                  color: Colors.grey,
+            cursorColor: Colors.black,
+            cursorHeight: 16,
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.only(
+                  left: 14, right: 3, top: 10, bottom: 10),
+              prefixIcon: const Icon(
+                Icons.search,
+                color: Colors.grey,
+                size: 22,
+              ),
+              hintText: 'Поиск',
+              hintStyle: const TextStyle(
+                fontSize: 18,
+              ),
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(14.0),
                 ),
-                hintText: 'Поиск',
-                hintStyle: TextStyle(
-                  fontSize: 18,
+                gapPadding: 0.5,
+              ),
+              focusedBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(14.0),
                 ),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(4.0)))),
+                gapPadding: 0.5,
+              ),
+              focusColor: Colors.white,
+              fillColor: Colors.grey.shade200,
+              filled: true,
+              hoverColor: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          // Кнопки "Сканировать QR" и "Добавить друга"
+          Row(
+            children: [
+              OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  fixedSize: const Size(185, 45),
+                  side: BorderSide(color: Colors.grey.shade300),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                  primary: Colors.grey.shade300,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    Icon(LineIcons.qrcode, size: 28, color: Colors.blue),
+                    SizedBox(
+                      width: 7,
+                    ),
+                    Text(
+                      'Сканировать QR',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  fixedSize: const Size(185, 45),
+                  side: BorderSide(color: Colors.grey.shade300),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                  primary: Colors.grey.shade300,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    Icon(
+                      LineIcons.userPlus,
+                      size: 28,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'Добавить друга',
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ],
       ),
